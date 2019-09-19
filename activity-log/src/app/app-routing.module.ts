86b02ba8597a251f62e83dd8f16ddac9c4b2aec0
Domalik Szymon <szymon.domalik@microsoft.wsei.edu.pl>
2019-09-19 13:59:41 +0200

    Added routing to new-activity

--- activity-log/src/app/app-routing.module.ts
+++ activity-log/src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth.guard';
+import { NewActivityComponent } from './new-activity/new-activity.component';
 
 
 const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path: 'new-activity',
+    component: NewActivityComponent
   },
   {
     path: '',
--- activity-log/src/app/new-activity/new-activity.component.ts
+++ activity-log/src/app/new-activity/new-activity.component.ts
@@ -1,10 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Exercises {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-new-activity',
   templateUrl: './new-activity.component.html',
   styleUrls: ['./new-activity.component.scss']
 })
+
+export class SelectExercise {
+  foods: Exercises[] = [
+    {value: 'Gym workout-0', viewValue: 'Gym workout'},
+    {value: 'Running-1', viewValue: 'Running'},
+    {value: 'Cycling-2', viewValue: 'Cycling'},
+    {value: 'Swimming-3', viewValue: 'Swimming'},
+    {value: 'Hiking-4', viewValue: 'Hiking'},
+    {value: 'Streching-5', viewValue: 'Streching'},
+    {value: 'Team sport-6', viewValue: 'Team sport'}
+  ];
+}
 export class NewActivityComponent implements OnInit {
 
   constructor() { }
