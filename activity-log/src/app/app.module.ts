623ecb897f7245aca056e9a285e1b9c8fa0a0510
Domalik Szymon <szymon.domalik@microsoft.wsei.edu.pl>
2019-09-19 13:34:42 +0200

    Genereted new component

--- activity-log/src/app/app.module.ts
+++ activity-log/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireModule } from 'angularfire2';
 import { environment } from 'src/environments/environment';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { NewActivityComponent } from './new-activity/new-activity.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    DashboardComponent
+    DashboardComponent,
+    NewActivityComponent
   ],
   imports: [
     BrowserModule,
