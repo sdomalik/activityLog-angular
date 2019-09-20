caf6847e72a3cae48c7a01e20bebb22d40c17399
Domalik Szymon <szymon.domalik@microsoft.wsei.edu.pl>
2019-09-20 17:31:27 +0200

    Added user interface and changed method

--- activity-log/src/app/auth.service.ts
+++ activity-log/src/app/auth.service.ts
@@ -1,39 +1,72 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { User } from 'firebase';
-import { Observable } from 'rxjs/index';
+import { User } from "./shared/services/user";
 import { Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { auth } from 'firebase/app';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  readonly authState$: Observable<User | null> = this.fireAuth.authState;
-  errorMessage: string = '';
-  
+  userData: any;
+
   constructor(
-    private fireAuth: AngularFireAuth,
-    public router: Router
-    ) { }
+    public fireStore: AngularFirestore,
+    public fireAuth: AngularFireAuth,
+    public router: Router,
+    public ngZone: NgZone
+    ) {
+      this.fireAuth.authState.subscribe(user => {
+        if (user){
+          this.userData = user;
+          localStorage.setItem('user', JSON.stringify(this.userData));
+          JSON.parse(localStorage.getItem('user'));
+        } else {
+          localStorage.setItem('user', null);
+          JSON.parse(localStorage.getItem('user'));
+        }
+      })
+     }
 
-  get user(): User | null {
-    return this.fireAuth.auth.currentUser;
-  }
-
-  login(email, password ) {
+    login(email, password ) {
     return this.fireAuth.auth.signInWithEmailAndPassword(email, password)
-    .then(() => this.router.navigate(['dashboard']))
-    .catch(err => console.log(err.message));
-  }
+    .then((result) => {
+      this.ngZone.run(() => {
+        this.router.navigate(['dashboard']);
+      });
+      this.SetUserData(result.user);
+      }).catch(error => window.alert(error.message));
+    }
 
-  register(email, password) {
-    return this.fireAuth.auth.createUserWithEmailAndPassword(email, password)
-    .then(() => this.router.navigate(['']))
-    .catch(err => console.log(err.message));
-  }
+    register(email, password) {
+      return this.fireAuth.auth.createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        this.ngZone.run(() => {
+          this.router.navigate(['']);
+        });
+        this.SetUserData(result.user)
+      }).catch(error => window.alert(error.message));
+    }
 
-  logout() {
-    return this.fireAuth.auth.signOut();
+    logout() {
+      return this.fireAuth.auth.signOut()
+      .then(() => {
+        localStorage.removeItem('user');
+        this.router.navigate(['']);
+      });
+    }
+
+    SetUserData(user) {
+      const userRef: AngularFirestoreDocument<any> = this.fireStore.doc(`users/${user.uid}`);
+      const userData: User = {
+        uid: user.uid,
+        email: user.email,
+        displayName: user.displayName,
+      }
+      return userRef.set(userData, {
+        merge: true
+      })
+    }
   }
-}
